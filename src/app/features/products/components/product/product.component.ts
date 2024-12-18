072b2c4c8fb634b5998b80d3d4d88a6365034b67
islam2021 <islamelsayed@gmail.com>
2024-12-18 16:38:09 +0200

    x

--- src/app/features/products/components/product/product.component.ts
+++ src/app/features/products/components/product/product.component.ts
@@ -1,28 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../../models/product';
 import { ProductService } from '../../services/product.service';
-import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [],
-
+  imports: [CommonModule],
   templateUrl: './product.component.html',
   styleUrl: './product.component.scss'
 })
 export class ProductComponent implements OnInit{
   products: Product[] = [];
 
-  constructor(private dataService: ProductService) {}
+  constructor(private dataService: ProductService, public http: HttpClient) {}
 
 
   ngOnInit() {
-    console.log('this.products');
     this.dataService.getProducts().subscribe((res: Product[]) => {
       this.products = res;
-      console.log(this.products);
     });
+
+
+
   }
+
+  // editQuantity(product: Product) {
+  //   this.dataService.editProductQuantity(product.ProductId, 9).subscribe(() => {
+  //     product.AvailablePieces = 9;
+  //   });
+  // }
 
 }
--- src/app/features/products/services/product.service.ts
+++ src/app/features/products/services/product.service.ts
@@ -7,13 +7,17 @@ import { Product } from '../models/product';
   providedIn: 'root'
 })
 export class ProductService {
-
-  productsUrl = 'https://github.com/islamelsayed93/order-master-dp/blob/main/src/app/assets/porducts.json'
+  // private productsUrl = 'https://raw.githubusercontent.com/islamelsayed93/order-master-dp/0312a2fcd45400f4eaa24e1518d6db3e572c2b7e/src/app/assets/porducts.json'
+  private productsUrl = '/assets/data/porducts.json'
 
   constructor(public http: HttpClient) { }
 
   getProducts(): Observable<Product[]> {
     return this.http.get<Product[]>(this.productsUrl);
+  }
+
+  editProductQuantity(productId: number, quantity: number): Observable<Product[]> {
+    return this.http.put<Product[]>(this.productsUrl, {productId, quantity });
   }
 
 
